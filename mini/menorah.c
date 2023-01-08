f054e25400ba8cdfdfd5dce8de701013d0f31eff
zanhecht <797985+zanhecht@users.noreply.github.com>
2023-01-08 04:26:59 -0500

    Light candles from newest to oldest
    On each day, candles are lit from newest to oldest. Also remove demo mode.

--- mini/menorah.c
+++ mini/menorah.c
@@ -2,9 +2,9 @@
 menorah.c
 
 Written by Windell Oskay, http://www.evilmadscientist.com/
+Modified by Zan Hecht, http://www.zansstuff.com/
 
 This is free software; please modify it and use it to do cool stuff.
-
 
 This avr-gcc program for the Atmel ATTiny2313 
 runs a very tiny hanukkah menorah that lites from 2 - 9 LED "candles."
@@ -14,26 +14,29 @@ runs a very tiny hanukkah menorah that lites from 2 - 9 LED "candles."
 The number of candles displayed is stored in EEPROM and is
 incremented each time that the unit is reset.
 
-After finishing the "standard" Hanukkah sequence, it then enters a demo mode,
-to show off its program.
-
 Version 2.0   Last Modified:  12/1/2008. 
 Now extra spiffy, with better LED uniformity, higher refresh rate, and lower power usage. :D
+ 
+Version 3.0   Last Modified: 1/8/2023. 
+Candles are lit in correct order (newest to oldest) each night. 
  
 */
 
 #include <avr/io.h>
 #include <avr/eeprom.h> 
 
+#define shortdelay(); 			asm("nop\n\t" \ 
+								"nop\n\t");
+
 uint16_t eepromWord __attribute__((section(".eeprom")));
 
 int main (void)
 	{
 	uint8_t PB, PD;		//8-bit unsigned integers
-	uint8_t PortBData[9] = {8U,9U,11U,15U,31U,63U,127U,255U,0};	//uint8_t multiplexed;
-	uint8_t PortDData[9] = {64U,64U,64U,64U,64U,64U,64U,64U,0};
+	uint8_t PortBData[9] = {128U,192U,224U,240U,244U,246U,247U,247U,255U};	//uint8_t multiplexed;
+	uint8_t PortDData[9] = {0,0,0,0,0,0,0,0,0};
 
-	unsigned int day, daycopy,  j;				//16-bit unsigned integers			
+	unsigned int day, position, i;				//16-bit unsigned integers
 
 	//Initialization routine: Clear watchdog timer-- this can prevent several things from going wrong.		
 	MCUSR &= 0xF7;		//Clear WDRF Flag
@@ -58,60 +61,144 @@ int main (void)
 		
 	day++;
 		
-	if (day > 8)	
+	if (day > 7)
 		day = 0U;
 
 	eeprom_write_word(&eepromWord, (uint8_t) day);
-
-	daycopy = day;
-		
-	/* Visualize outputs:
-
-			B7 B6 B5 B4 B3 B2 B1 B0 D6	 B value
-	Day 0	0  0  0  0  1  0  0  0  1		8
-	Day 1	0  0  0  0  1  0  0  1  1		9
-	Day 2	0  0  0  0  1  0  1  1  1		11
-	Day 3   0  0  0  0  1  1  1  1  1		15
-	Day 4	0  0  0  1  1  1  1  1  1		31
-	Day 5   0  0  1  1  1  1  1  1  1		63
-	Day 6   0  1  1  1  1  1  1  1  1		127
-	Day 7   1  1  1  1  1  1  1  1  1		255
-
-	Day 8: Demo mode, scan day = 0 to 7.
-
-	Pin D6 is always high, other D pins are always low. 2^6 = 64, so D = 64U
-
-	PORTB: B3 is always high.
-	Summarize B values in data table;
-	uint8_t PortBData[9] = {8U,9U,11U,15U,31U,63U,127U,255U,0};
-
-	We add a final case-- all lights off-- as well.
-
-	*/
 
 	PORTA = 0;
 	PORTB = 0;
 	PORTD = 0;
 
+	/* Visualize outputs:
+
+				B7 B6 B5 B4 B3 B2 B1 B0 D6		B value	D value
+
+	Day 0	0  0  0  0  0  0  0  0  1		0			64
+	Day 0	0  0  0  0  1  0  0  0  1		8			64
+
+	Day 1	0  0  0  0  0  0  0  1  0		1			0
+	Day 1	0  0  0  0  0  0  0  1  1		1			64
+	Day 1	0  0  0  0  1  0  0  1  1		9			64
+
+	Day 2	0  0  0  0  0  0  1  0  0		2			0
+	Day 2	0  0  0  0  0  0  1  1  0		3			0
+	Day 2	0  0  0  0  0  0  1  1  1		3			64
+	Day 2	0  0  0  0  1  0  1  1  1		11			64
+
+	Day 3	0  0  0  0  0  1  0  0  0		4			0
+	Day 3	0  0  0  0  0  1  1  0  0		6			0
+	Day 3	0  0  0  0  0  1  1  1  0		7			0
+	Day 3	0  0  0  0  0  1  1  1  1		7			64
+	Day 3	0  0  0  0  1  1  1  1  1		15			64
+
+	Day 4	0  0  0  1  0  0  0  0  0		16			0
+	Day 4	0  0  0  1  0  1  0  0  0		20			0
+	Day 4	0  0  0  1  0  1  1  0  0		22			0
+	Day 4	0  0  0  1  0  1  1  1  0		23			0
+	Day 4	0  0  0  1  0  1  1  1  1		23			64
+	Day 4	0  0  0  1  1  1  1  1  1		31			64
+
+	Day 5	0  0  1  0  0  0  0  0  0		32			0
+	Day 5	0  0  1  1  0  0  0  0  0		48			0
+	Day 5	0  0  1  1  0  1  0  0  0		52			0
+	Day 5	0  0  1  1  0  1  1  0  0		54			0
+	Day 5	0  0  1  1  0  1  1  1  0		55			0
+	Day 5	0  0  1  1  0  1  1  1  1		55			64
+	Day 5	0  0  1  1  1  1  1  1  1		63			64
+
+	Day 6	0  1  0  0  0  0  0  0  0		64			0
+	Day 6	0  1  1  0  0  0  0  0  0		96			0
+	Day 6	0  1  1  1  0  0  0  0  0		112		0
+	Day 6	0  1  1  1  0  1  0  0  0		116		0
+	Day 6	0  1  1  1  0  1  1  0  0		118		0
+	Day 6	0  1  1  1  0  1  1  1  0		119		0
+	Day 6	0  1  1  1  0  1  1  1  1		119		64
+	Day 6	0  1  1  1  1  1  1  1  1		127		64
+
+	Day 7	1  0  0  0  0  0  0  0  0		128		0
+	Day 7	1  1  0  0  0  0  0  0  0		192		0
+	Day 7	1  1  1  0  0  0  0  0  0		224		0
+	Day 7	1  1  1  1  0  0  0  0  0		240		0
+	Day 7	1  1  1  1  0  1  0  0  0		244		0
+	Day 7	1  1  1  1  0  1  1  0  0		246		0
+	Day 7	1  1  1  1  0  1  1  1  0		247		0
+	Day 7	1  1  1  1  0  1  1  1  1		247		64
+	Day 7	1  1  1  1  1  1  1  1  1		255		64
+
+	Pin D6 high when Day 1 candle is lit, other D pins are always low. 2^6 = 64, so D = 64U
+
+	PORTB: B3 represents shamash and is always high only in final case.
+
+	*/
+	// set first 7 candles (plus shamash) depending on the day
+	switch(day) {
+		case 0 :
+			PortBData[0] = 0;
+			PortBData[1] = 8U;
+			break;
+		case 1 :
+			PortBData[0] = 1U;
+			PortBData[1] = 1U;
+			PortBData[2] = 9U;
+			break;
+		case 2 :
+			PortBData[0] = 2U;
+			PortBData[1] = 3U;
+			PortBData[2] = 3U;
+			PortBData[3] = 11U;
+			break;
+		case 3 :
+			PortBData[0] = 4U;
+			PortBData[1] = 6U;
+			PortBData[2] = 7U;
+			PortBData[3] = 7U;
+			PortBData[4] = 15U;
+			break;
+		case 4 :
+			PortBData[0] = 16U;
+			PortBData[1] = 20U;
+			PortBData[2] = 22U;
+			PortBData[3] = 23U;
+			PortBData[4] = 23U;
+			PortBData[5] = 31U;
+			break;
+		case 5 : 
+			PortBData[0] = 32U;
+			PortBData[1] = 48U;
+			PortBData[2] = 52U;
+			PortBData[3] = 54U;
+			PortBData[4] = 55U;
+			PortBData[5] = 55U;
+			PortBData[6] = 63U;
+			break;
+		case 6 :
+			PortBData[0] = 64U;
+			PortBData[1] = 96U;
+			PortBData[2] = 112U;
+			PortBData[3] = 116U;
+			PortBData[4] = 118U;
+			PortBData[5] = 119U;
+			PortBData[6] = 119U;
+			PortBData[7] = 127U;
+		// case 7 is the default value described above by
+		// uint8_t PortBData[9] = {128U,192U,224U,240U,244U,246U,247U,247U,255U};
+	}
+	
+	// turn on last candle for last two positions
+	PortDData[day] = 64U;
+	PortDData[day+1] = 64U;
+
+	position = 0;
 	for (;;)  // main loop
-
 		{
-		if (daycopy == 8)
-		{
-			day++;
-			if (day > 8)
-				day = 0;
-		}
 
-		PB = PortBData[day];
-		PD = PortDData[day];
-
-		j = 0;
-		while (j < 15000)		// This sets how long it is between changing number of "candles" when (daycopy == 8)	
+		PB = PortBData[position];
+		PD = PortDData[position];
+		
+		i = 0;
+		while (i < 15000)		// This sets how long it is between changing number of "candles"
 			{
-
-			#define shortdelay(); 			asm("nop\n\t" \ 
-											"nop\n\t");
 
 			//Multiplexing routine: Each LED is on (1/9) of the time. 
 			//  -> Uses much less power.
@@ -120,41 +207,44 @@ int main (void)
 			shortdelay();
 			PORTD = 0;
 
-			PORTB = PB & (1);
+			PORTB = PB & (1U);
 			shortdelay();
 			PORTB = 0;
 
-			PORTB = PB & (2);
+			PORTB = PB & (2U);
 			shortdelay();
 			PORTB = 0;
 
-			PORTB = PB & (4);
+			PORTB = PB & (4U);
 			shortdelay();
 			PORTB = 0;
 
-			PORTB = PB & (8);
+			PORTB = PB & (8U);
 			shortdelay();
 			PORTB = 0;
 
-			PORTB = PB & (16);
+			PORTB = PB & (16U);
 			shortdelay();
 			PORTB = 0;
 
-			PORTB = PB & (32);
+			PORTB = PB & (32U);
 			shortdelay();
 			PORTB = 0;
 
-			PORTB = PB & (64);
+			PORTB = PB & (64U);
 			shortdelay();
 			PORTB = 0;
 
-			PORTB = PB & (128);
+			PORTB = PB & (128U);
 			shortdelay();
 			PORTB = 0;
 
-			j++;				
+			i++;
 			} 
 			
+		if ( position < (day + 1) )
+			position++;
+		
 		}	//End main loop
 		return 0;
 	}
