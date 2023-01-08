c6a74065915f6dd1d2b95f1336d5b86369b7177f
zanhecht <797985+zanhecht@users.noreply.github.com>
2023-01-08 13:08:40 -0500

    Remove unnecessary tab

--- mini/makeinstall.bat
+++ mini/makeinstall.bat
@@ -1,4 +1,4 @@
-@PATH=%path%C:\Program Files\avr8-gnu-toolchain-win32_x86_64\bin;C:\Program Files (x86)\GnuWin32\bin;C:\Program Files\avrdude	;
+@PATH=%path%C:\Program Files\avr8-gnu-toolchain-win32_x86_64\bin;C:\Program Files (x86)\GnuWin32\bin;C:\Program Files\avrdude;
 make -f Makefile
 make -f Makefile install
 del *.bin *.elf *.hex *.lst *.map *.o *.srec
